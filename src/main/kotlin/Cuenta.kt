1c71c0006ca0047b124247df3ff30056bd77246d
LuciaES <lespsan543@g.educaand.es>
2023-01-26 21:11:45 +0100

    Initial commit

---
+++ src/main/kotlin/Cuenta.kt
@@ -0,0 +1,4 @@
+class Cuenta (numero : Int, cantidad : Int){
+    val numero_cuenta = numero
+    val saldo = cantidad
+}
---
+++ src/main/kotlin/Persona.kt
@@ -0,0 +1,9 @@
+class Persona(dni : String) {
+    val DNI = dni
+    var listas_cuentas = mutableListOf<Cuenta>()
+    var maxCuentas = 3
+
+    fun añadir_cuenta(){
+        
+    }
+}
